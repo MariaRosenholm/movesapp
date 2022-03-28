a36382fce1ec588af8456f33a0b306b95f7aa9c0
NooraJumppanen <noora.jumppanen@gmail.com>
2022-03-28 16:29:45 +0300

    added loading spinner to dancemove and search pages

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,40 +1,52 @@
-import { useState } from "react";
-import ListItem from "./ListItem";
+import { useState, useEffect } from 'react';
+import ListItem from './ListItem';
+import Loader from './Loader';
 
 const Search = ({ dancelist }) => {
-  const [searchkey, setSearchKey] = useState("");
+	const [isLoading, setIsLoading] = useState(true);
+	const [searchkey, setSearchKey] = useState('');
 
-  const searchInputHandler = (event) => {
-    setSearchKey(event.target.value);
-  };
+	useEffect(() => {
+		setIsLoading(false);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  let filteredDanceMoves = dancelist.filter((dancemove) => {
-    const key = searchkey.toLowerCase();
-    const move = dancemove.Move.toLowerCase();
-    const creator = dancemove.Creator.toLowerCase();
-    return move.includes(key) || creator.includes(key);
-  });
+	const searchInputHandler = (event) => {
+		setSearchKey(event.target.value);
+	};
 
-  return (
-    <div className="search">
-      <div className="searchWrapper">
-        <input
-          placeholder="search"
-          onChange={searchInputHandler}
-          className="searchValue" // onClick={deleteSearch}
-        />
-      </div>
-      {filteredDanceMoves.map((dancemove) => (
-        <ListItem
-          key={dancemove.Id}
-          move={dancemove.Move}
-          creator={dancemove.Creator}
-          Id={dancemove.Id}
-          link={dancemove.Link}
-        />
-      ))}
-    </div>
-  );
+	let filteredDanceMoves = dancelist.filter((dancemove) => {
+		const key = searchkey.toLowerCase();
+		const move = dancemove.Move.toLowerCase();
+		const creator = dancemove.Creator.toLowerCase();
+		return move.includes(key) || creator.includes(key);
+	});
+
+	return (
+		<div className="search">
+			<div className="searchWrapper">
+				<input
+					placeholder="search"
+					onChange={searchInputHandler}
+					className="searchValue" // onClick={deleteSearch}
+				/>
+			</div>
+			{isLoading && <Loader />}
+			{!isLoading && (
+				<div>
+					{filteredDanceMoves.map((dancemove) => (
+						<ListItem
+							key={dancemove.Id}
+							move={dancemove.Move}
+							creator={dancemove.Creator}
+							Id={dancemove.Id}
+							link={dancemove.Link}
+						/>
+					))}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Search;
