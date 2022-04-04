822b462aee791afc387f5d522df9ba4bc57780c4
MariaRosenholm <maria.rosenholm@gmail.com>
2022-04-04 22:30:23 +0300

    dancemove component styles updated to support the ig video

--- src/components/DanceMove.js
+++ src/components/DanceMove.js
@@ -10,31 +10,33 @@ import InstagramEmbed from "react-instagram-embed";
 function DanceMove({ dancelist }) {
   const [isLoading, setIsLoading] = useState();
   const [notes, setNotes] = useState();
+  const [by, setBy] = useState();
   const params = useParams();
 
   let move = dancelist.find((move) => move.Id === +params.id);
   let link = move?.Link;
 
   useEffect(() => {
-    move?.HOX ? setNotes(move.HOX) : setNotes("No notes added.");
+    move?.HOX ? setNotes(move.HOX) : setNotes("No notes added");
     !link ? setIsLoading(false) : setIsLoading(true);
-  }, [link, move?.HOX]);
+    move.Creator ? setBy(true) : setBy(false);
+  }, [link, move?.HOX, move?.Creator]);
 
   return (
     <div className="DanceMove">
       <Link to={"/edit/" + params.id} className="squre-pen-wrapper">
         <FontAwesomeIcon icon={faEdit} className="squre-pen" />
       </Link>
-
-      <h3>{move?.Move}</h3>
-      <p>by</p>
-      <h3>{move?.Creator}</h3>
-
+      <div className="nameAndCreator">
+        <h3 className="moveName">{move?.Move}</h3>
+        {by && <p>by</p>}
+        <h3>{move?.Creator}</h3>
+      </div>
+      <p className="notes">
+        <span>{notes}</span>
+      </p>
       <div id="notesAndIGWrapper">
-        <p>
-          <span>{notes}</span>
-        </p>
-        {!link && <h3>No instagram video added!</h3>}
+        {!link && <p>No instagram video added!</p>}
         {isLoading && <Loader />}
         {link && (
           <InstagramEmbed
@@ -50,9 +52,7 @@ function DanceMove({ dancelist }) {
               setIsLoading(false);
             }}
             onAfterRender={() => {}}
-            onFailure={() => {
-              <h3>Instagram video cannot be shown!</h3>;
-            }}
+            onFailure={() => {}}
           />
         )}
       </div>
